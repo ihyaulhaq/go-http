27a05a58cbab790e11a082ffb287cf32de58197d
ihyaulhaq <ahmad.ihyaulhaq@gmail.com>
2026-03-02 14:00:06 +0700

    feat: make tcp connectiion

--- main.go
+++ main.go
@@ -5,28 +5,40 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
+
+	// "os"
 	"strings"
 )
 
-const inputFilePath = "messages.txt"
+// const inputFilePath = "messages.txt"
+const port = ":42069"
 
 func main() {
-	file, err := os.Open(inputFilePath)
+	// file, err := os.Open(inputFilePath)
+
+	listener, err := net.Listen("tcp", port)
 
 	if err != nil {
-		log.Fatalf("cant open file %s : %s\n", inputFilePath, err)
+		log.Fatalf("cant lisen to port %s : %s\n", port, err)
 	}
-	str := getLinesChannel(file)
+	defer listener.Close()
+	defer fmt.Println("Connention has been close")
 
-	fmt.Printf("Reading data from %s\n", inputFilePath)
-	fmt.Println("=====================================")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Connention has been made ")
+		str := getLinesChannel(conn)
 
-	for line := range str {
+		fmt.Println("=====================================")
 
-		fmt.Printf("read: %s\n", line)
+		for line := range str {
+			fmt.Println(line)
+		}
 	}
-
 }
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
@@ -44,7 +56,6 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 			if err != nil {
 				if curentLine != "" {
 					lines <- curentLine
-					curentLine = ""
 				}
 
 				if errors.Is(err, io.EOF) {
@@ -56,13 +67,11 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 			}
 
 			str := string(buffer[:n])
-
 			parts := strings.Split(str, "\n")
 
 			for l := 0; l < len(parts)-1; l++ {
 
 				line := curentLine + parts[l]
-
 				lines <- line
 				curentLine = ""
 			}
